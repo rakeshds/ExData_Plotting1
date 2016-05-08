1ec0189ff9f465743c77d352b92f6ce72606989e
rakeshds <rakeshds@users.noreply.github.com>
2016-05-08 20:26:09 +0530

    code for plot3 with generated plot

---
+++ plot3.R
@@ -0,0 +1,20 @@
+##downloading the data and unzipping
+
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+f<-file.path(getwd(),"hopoco.zip")
+download.file(url,f)
+unzip(f,exdir = getwd())
+
+##Reading data into R
+fp=file.path(getwd(),"hopowcon.txt")
+houpowcon<-read.table(text=grep("^[1,2]/2/2007",readLines(fp),value = TRUE),sep=';',col.names =c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+##Creating energy submeter plot
+houpowcon$Date <- as.Date(houpowcon$Date, format = 'dd/mm/YYYY')
+houpowcon$DateTime <- strptime(paste(houpowcon$Date, houpowcon$Time),format = "%Y-%m-%d %H:%M:%S",tz="")
+png(filename = "./plot3.png",width = 480,height = 480,units = "px")
+with(houpowcon,plot(houpowcon$DateTime, houpowcon$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l'))
+lines(houpowcon$DateTime, houpowcon$Sub_metering_2,col="red")
+lines(houpowcon$DateTime, houpowcon$Sub_metering_3,col="blue")
+dev.off()
+
